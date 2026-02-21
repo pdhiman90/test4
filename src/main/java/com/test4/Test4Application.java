93bbe74b400aa39cb3e52028f41a58ca82a6785d
Prince Dhiman <112814548+pdhiman90@users.noreply.github.com>
2026-02-21 15:52:24 +0530

    Add multiple variable declarations in main method

--- src/main/java/com/test4/Test4Application.java
+++ src/main/java/com/test4/Test4Application.java
@@ -7,6 +7,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test4Application {
 
 	public static void main(String[] args) {
+		String name = "mike";
+		int x = 100;
+		boolean isPresent=false;
+		int y = 200;
+		char z = 'a';
+		double k = 10.3;
+		float m = 10.3f;
+		char ch = 'x';
+		boolean isSubscribed = true;
+		
 		SpringApplication.run(Test4Application.class, args);
 	}
 
