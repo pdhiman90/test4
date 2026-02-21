4c41b1e3620fa8af3b5fa1570e8f1933315e34b8
Prince <princedhiman73573@gmail.com>
2026-02-21 12:15:49 +0530

    add isSubsribed variable

--- src/main/java/com/test4/Test4Application.java
+++ src/main/java/com/test4/Test4Application.java
@@ -14,6 +14,7 @@ public class Test4Application {
 		char z = 'a';
 		double k = 10.3;
 		float m = 10.3f;
+		boolean isSubscribed = true;
 		SpringApplication.run(Test4Application.class, args);
 	}
 
